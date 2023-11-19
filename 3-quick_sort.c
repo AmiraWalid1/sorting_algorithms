065f8336d2dc9148bf14325c6f6218db429d307b
Amira Walid <amirawaleed012@gmail.com>
2023-11-19 16:01:30 +0200

    quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,76 @@
+#include "sort.h"
+/**
+ * partition - the Lomuto partition scheme to make guick sort.
+ * @array: array.
+ * @size: size of array.
+ * @low: low index of sequence of array will be sorted.
+ * @hight: hight index of sequence of array will be sorted.
+ * Return: index of pivot.
+*/
+int partition(int *array, size_t size, int low, int hight)
+{
+	int pivot = array[hight];
+	int i = low - 1, j;
+
+	for (j = low ; j <= hight ; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				swap(array + i, array + j);
+				print_array((const int *)array, size);
+			}
+		}
+	}
+	return (i);
+
+}
+
+
+/**
+ * quick_sort_rec - function that sorts an array of integers in
+ * ascending order using the Quick sort algorithm.
+ * @array: array.
+ * @size: size of array.
+ * @low: low index of sequence of array will be sorted.
+ * @hight: hight index of sequence of array will be sorted.
+ * Return: void
+*/
+void quick_sort_rec(int *array, size_t size, int low, int hight)
+{
+	if (low < hight)
+	{
+		int pivot = partition(array, size, low, hight);
+
+		quick_sort_rec(array, size, low, pivot - 1);
+		quick_sort_rec(array, size, pivot + 1, hight);
+	}
+}
+
+
+/**
+ * quick_sort - function call quick_sort_rec.
+ * @array: array
+ * @size: size of array
+ * Return: void
+*/
+void quick_sort(int *array, size_t size)
+{
+	quick_sort_rec(array, size, 0, size - 1);
+}
+
+
+/**
+ * swap - swap the value pointed by  two pointer
+ * @a: first pointer
+ * @b: second pointer
+ * Return: void
+*/
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
--- sort.h
+++ sort.h
@@ -17,6 +17,14 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+/*insertion_sort */
 void insertion_sort_list(listint_t **list);
 
+/*quick_sort*/
+void quick_sort(int *array, size_t size);
+int partition(int *array, size_t size, int low, int hight);
+void quick_sort_rec(int *array, size_t size, int low, int hight);
+
+void swap(int *a, int *b);
 #endif /*SORT_H*/
